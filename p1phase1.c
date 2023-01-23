81edbf4561bf169de57a8571bb546411aab2b984
bhrdj <20behrad04@gmail.com>
2023-01-23 20:50:50 +0330

    createfile

--- p1phase1.c
+++ p1phase1.c
@@ -16,7 +16,7 @@ void invalid_command() {
 void crfile_withoutspace() {
     //printf("ok") ;
     int slashcounter = 0, i, j, flag ;
-    char wsname[100], compeletename[100] = "root/", dirname[100] = "root/" ;
+    char wsname[100], completename[100] = "root/", dirname[100] = "root/" ;
     getstr(wsname) ;
     for(i = 0 ; wsname[i] != '\0' ; i++)
         if(wsname[i] == '/')
@@ -27,27 +27,67 @@ void crfile_withoutspace() {
             if (wsname[j] == '/'){
                 if(++flag >= i + 1)
                     break ;
+                else
+                    dirname[j + 5] = wsname[j] ;
             }
             else
                 dirname[j + 5] = wsname[j] ;
         }
         mkdir(dirname) ;
+        strcpy(dirname, "root/") ;
     }
     for(i = 0 ; wsname[i] != '\0' ; i++)
-        compeletename[i + 5] = wsname[i] ;
+        completename[i + 5] = wsname[i] ;
     FILE *newfile ;
-    if(newfile = fopen(compeletename, "r")) {
-        printf("file already exists!") ;
+    if(newfile = fopen(completename, "r")) {
+        printf("file already exists!\n") ;
     }
     else {
-        newfile = fopen(compeletename, "w") ;
+        newfile = fopen(completename, "w") ;
         printf("new file created successfuly!\n") ;
     }
     fclose(newfile) ;
 }
 
 void crfile_withspace() {
-    
+    char c, completename[100] = "root/", dirname[100] = {0} ;
+    int i = 0, slashcounter = 0, flag, j ;
+    scanf("%c", &c) ;
+    while(c != '\"') {
+        completename[i + 5] = c ;
+        if(c == '/')
+            slashcounter++ ;
+        i++ ;
+        scanf("%c", &c) ;
+    }
+    /*i = 0 ;
+    while(completename[i] != '\"'){
+        printf("%c", completename[i]) ;
+        i++ ;
+    }*/
+    for (i = 1 ; i < slashcounter + 1 ; i++){
+        flag = 0 ;
+        for ( j = 0 ; flag < i + 1 ; j++){
+            if (completename[j] == '/'){
+                if(++flag >= i + 1)
+                    break ;
+                else
+                    dirname[j] = completename[j] ;
+            }
+            else
+                dirname[j] = completename[j] ;
+        }
+        mkdir(dirname) ;
+    }
+    FILE *newfile ;
+    if(newfile = fopen(completename, "r")) {
+        printf("file already exists!\n") ;
+    }
+    else {
+        newfile = fopen(completename, "w") ;
+        printf("new file created successfuly!\n") ;
+    }
+    fclose(newfile) ;
 }
 
 void createfile() {
